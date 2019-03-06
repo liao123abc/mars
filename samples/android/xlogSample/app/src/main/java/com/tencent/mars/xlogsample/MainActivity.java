de6e4b950b7cdd102b1481769a159023b24b1a23
liaodongming <thomasliao@126.com>
2019-03-06 14:27:33 +0800

    fix build

--- samples/android/xlogSample/app/src/main/java/com/tencent/mars/xlogsample/MainActivity.java
+++ samples/android/xlogSample/app/src/main/java/com/tencent/mars/xlogsample/MainActivity.java
@@ -18,6 +18,7 @@ import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 import com.tencent.mars.xlog.Xlog;
@@ -31,13 +32,32 @@ public class MainActivity extends AppCompatActivity {
 
 
         String logPath = Environment.getExternalStorageDirectory().getPath() + "/logsample/xlog";
+        String publicKey = "572d1e2710ae5fbca54c76a382fdd44050b3a675cb2bf39feebe85ef63d947aff0fa4943f1112e8b6af34bebebbaefa1a0aae055d9259b89a1858f7cc9af9df1";
         Log.d("test", logPath);
         Xlog.setConsoleLogOpen(true);
-        Xlog.appenderOpen(Xlog.LEVEL_DEBUG, Xlog.AppednerModeAsync, "", logPath, "LOGSAMPLE", 0, "");
 
+        //appenderOpen(int level, int mode, String cacheDir, String logDir, String nameprefix, int cacheDays, String pubkey);
+
+        Xlog.appenderOpen(
+                Xlog.LEVEL_DEBUG,
+                Xlog.AppednerModeAsync,//异步还是同步
+                "", //cacheDir
+                logPath, //logDir
+                "LOGSAMPLE", //nameprefix
+                0, // cacheDays
+                publicKey //publikey
+        );
+        com.tencent.mars.xlog.Log.setLogImp(new Xlog());
 
         TextView tv = (TextView) findViewById(R.id.sample_text);
         tv.setText(stringFromJNI());
+
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stringFromJNI();
+            }
+        });
     }
 
     /**
